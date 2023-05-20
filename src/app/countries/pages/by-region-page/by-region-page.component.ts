09aff81cde26e0f29eb4414f373b1bdb72a8bd8c
Sebastian Rios <sebastianriosrico@gmail.com>
2023-05-20 18:46:23 -0500

    First develop of the project (easy level)

---
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
+
+@Component({
+  selector: 'app-by-region-page',
+  templateUrl: './by-region-page.component.html',
+  styles: [
+  ]
+})
+export class ByRegionPageComponent {
+
+  public countries: Country[] = [];
+
+  constructor(private contriesService: CountriesService) {}
+
+  searchByRegion(term: string): void {
+    this.contriesService.searchByRegion(term)
+      .subscribe(countries => {
+        this.countries = countries;
+      })
+  }
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+
+
+
+@NgModule({
+  declarations: [
+    AboutPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent,
+    SearchBoxComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    AboutPageComponent,
+    HomePageComponent,
+    ContactPageComponent,
+    SidebarComponent,
+    SearchBoxComponent,
+  ]
+})
+export class SharedModule { }
